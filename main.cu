e4b7d135b6d10504acd18a42e278c44b9616b616
Nol Moonen <nolmoonen@hotmail.nl>
2024-07-03 22:03:30 +0200

    macros

--- main.cu
+++ main.cu
@@ -50,80 +50,21 @@ struct hash_t {
     uint32_t arr[8];
 };
 
-__forceinline__ __device__ __host__ uint32_t rotr(uint32_t a, int b)
-{
-#if __CUDA_ARCH__ and 0
-    // seems to be worse, looks like the compiler can already figure this out
-    return __funnelshift_r(a, a, b);
-#else
-    return (a >> b) | (a << (32 - b));
-#endif
-}
-__forceinline__ __device__ __host__ uint32_t ch(uint32_t x, uint32_t y, uint32_t z)
-{
-    // return (x & y) ^ (~x & z);
-    // https://github.com/hashcat/hashcat/blob/master/OpenCL/inc_hash_sha256.h
-    return z ^ (x & (y ^ z));
-}
-__forceinline__ __device__ __host__ uint32_t maj(uint32_t x, uint32_t y, uint32_t z)
-{
-    // return (x & y) ^ (x & z) ^ (y & z);
-    // https://github.com/hashcat/hashcat/blob/master/OpenCL/inc_hash_sha256.h
-    return (x & y) | (z & (x ^ y));
-}
-__forceinline__ __device__ __host__ uint32_t ep0(uint32_t x)
-{
-    return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
-}
-__forceinline__ __device__ __host__ uint32_t ep1(uint32_t x)
-{
-    return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
-}
-__forceinline__ __device__ __host__ uint32_t sig0(uint32_t x)
-{
-    return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
-}
-__forceinline__ __device__ __host__ uint32_t sig1(uint32_t x)
-{
-    return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
-}
+#define SHA256_ROTR(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
+#define SHA256_MAJ(x, y, z) (((x) & (y)) | ((z) & ((x) ^ (y))))
+#define SHA256_CH(x, y, z) ((z) ^ ((x) & ((y) ^ (z))))
 
-__forceinline__ __device__ __host__ void sha256_round(
-    uint32_t m,
-    uint32_t k,
-    uint32_t& a,
-    uint32_t& b,
-    uint32_t& c,
-    uint32_t& d,
-    uint32_t& e,
-    uint32_t& f,
-    uint32_t& g,
-    uint32_t& h)
-{
-    const uint32_t t1 = h + ep1(e) + ch(e, f, g) + k + m;
-    const uint32_t t2 = ep0(a) + maj(a, b, c);
+#define SHA256_EP0(x) (SHA256_ROTR(x, 2) ^ SHA256_ROTR(x, 13) ^ SHA256_ROTR(x, 22))
+#define SHA256_EP1(x) (SHA256_ROTR(x, 6) ^ SHA256_ROTR(x, 11) ^ SHA256_ROTR(x, 25))
+#define SHA256_SIG0(x) (SHA256_ROTR(x, 7) ^ SHA256_ROTR(x, 18) ^ (x >> 3))
+#define SHA256_SIG1(x) (SHA256_ROTR(x, 17) ^ SHA256_ROTR(x, 19) ^ (x >> 10))
 
-    h = g;
-    g = f;
-    f = e;
-    e = d + t1;
-    d = c;
-    c = b;
-    b = a;
-    a = t1 + t2;
-}
+#define SHA256_STEP(m, k, a, b, c, d, e, f, g, h)                                                  \
+    h += SHA256_EP1(e) + SHA256_CH(e, f, g) + k + m;                                               \
+    d += h;                                                                                        \
+    h += SHA256_EP0(a) + SHA256_MAJ(a, b, c);
 
-__forceinline__ __device__ __host__ uint32_t
-sha256_update_m_(uint32_t i2, uint32_t i7, uint32_t i15, uint32_t i16)
-{
-    return sig1(i2) + i7 + sig0(i15) + i16;
-}
-
-__forceinline__ __device__ __host__ void sha256_update_m(uint32_t (&m)[16], int i)
-{
-    m[i] = sha256_update_m_(
-        m[(i + 16 - 2) % 16], m[(i + 16 - 7) % 16], m[(i + 16 - 15) % 16], m[(i + 16 - 16) % 16]);
-}
+#define SHA256_EXPAND(i2, i7, i15, i16) (SHA256_SIG1(i2) + i7 + SHA256_SIG0(i15) + i16)
 
 __forceinline__ __device__ __host__ uint32_t swap_endian(uint32_t x)
 {
@@ -165,59 +106,141 @@ __forceinline__ __device__ __host__ void sha256(
     uint32_t g = gg;
     uint32_t h = hh;
 
-    // finish the first 16 rounds using precalculated data
-    sha256_round(m00, k[0], a, b, c, d, e, f, g, h);
-    sha256_round(m01, k[1], a, b, c, d, e, f, g, h);
-    sha256_round(m02, k[2], a, b, c, d, e, f, g, h);
-    sha256_round(m03, k[3], a, b, c, d, e, f, g, h);
-    sha256_round(m04, k[4], a, b, c, d, e, f, g, h);
-    sha256_round(m05, k[5], a, b, c, d, e, f, g, h);
-    sha256_round(m06, k[6], a, b, c, d, e, f, g, h);
-    sha256_round(m07, k[7], a, b, c, d, e, f, g, h);
-    sha256_round(m08, k[8], a, b, c, d, e, f, g, h);
-    sha256_round(m09, k[9], a, b, c, d, e, f, g, h);
-    sha256_round(m10, k[10], a, b, c, d, e, f, g, h);
-    sha256_round(m11, k[11], a, b, c, d, e, f, g, h);
-    sha256_round(m12, k[12], a, b, c, d, e, f, g, h);
-    sha256_round(m13, k[13], a, b, c, d, e, f, g, h);
-    sha256_round(m14, k[14], a, b, c, d, e, f, g, h);
-    sha256_round(m15, k[15], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m00, k[0x00], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m01, k[0x01], h, a, b, c, d, e, f, g);
+    SHA256_STEP(m02, k[0x02], g, h, a, b, c, d, e, f);
+    SHA256_STEP(m03, k[0x03], f, g, h, a, b, c, d, e);
+    SHA256_STEP(m04, k[0x04], e, f, g, h, a, b, c, d);
+    SHA256_STEP(m05, k[0x05], d, e, f, g, h, a, b, c);
+    SHA256_STEP(m06, k[0x06], c, d, e, f, g, h, a, b);
+    SHA256_STEP(m07, k[0x07], b, c, d, e, f, g, h, a);
+    SHA256_STEP(m08, k[0x08], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m09, k[0x09], h, a, b, c, d, e, f, g);
+    SHA256_STEP(m10, k[0x0a], g, h, a, b, c, d, e, f);
+    SHA256_STEP(m11, k[0x0b], f, g, h, a, b, c, d, e);
+    SHA256_STEP(m12, k[0x0c], e, f, g, h, a, b, c, d);
+    SHA256_STEP(m13, k[0x0d], d, e, f, g, h, a, b, c);
+    SHA256_STEP(m14, k[0x0e], c, d, e, f, g, h, a, b);
+    SHA256_STEP(m15, k[0x0f], b, c, d, e, f, g, h, a);
 
-    uint32_t m16 = sha256_update_m_(m14, m09, m01, m00);
-    uint32_t m17 = sha256_update_m_(m15, m10, m02, m01);
-    uint32_t m18 = sha256_update_m_(m16, m11, m03, m02);
-    uint32_t m19 = sha256_update_m_(m17, m12, m04, m03);
-    uint32_t m20 = sha256_update_m_(m18, m13, m05, m04);
-    uint32_t m21 = sha256_update_m_(m19, m14, m06, m05);
-    uint32_t m22 = sha256_update_m_(m20, m15, m07, m06);
-    uint32_t m23 = sha256_update_m_(m21, m16, m08, m07);
-    uint32_t m24 = sha256_update_m_(m22, m17, m09, m08);
-    uint32_t m25 = sha256_update_m_(m23, m18, m10, m09);
-    uint32_t m26 = sha256_update_m_(m24, m19, m11, m10);
-    uint32_t m27 = sha256_update_m_(m25, m20, m12, m11);
-    uint32_t m28 = sha256_update_m_(m26, m21, m13, m12);
-    uint32_t m29 = sha256_update_m_(m27, m22, m14, m13);
-    uint32_t m30 = sha256_update_m_(m28, m23, m15, m14);
-    uint32_t m31 = sha256_update_m_(m29, m24, m16, m15);
+    uint32_t m0 = m00;
+    uint32_t m1 = m01;
+    uint32_t m2 = m02;
+    uint32_t m3 = m03;
+    uint32_t m4 = m04;
+    uint32_t m5 = m05;
+    uint32_t m6 = m06;
+    uint32_t m7 = m07;
+    uint32_t m8 = m08;
+    uint32_t m9 = m09;
+    uint32_t ma = m10;
+    uint32_t mb = m11;
+    uint32_t mc = m12;
+    uint32_t md = m13;
+    uint32_t me = m14;
+    uint32_t mf = m15;
 
-    uint32_t m[16] = {
-        m16, m17, m18, m19, m20, m21, m22, m23, m24, m25, m26, m27, m28, m29, m30, m31};
+    m0 = SHA256_EXPAND(me, m9, m1, m0);
+    m1 = SHA256_EXPAND(mf, ma, m2, m1);
+    m2 = SHA256_EXPAND(m0, mb, m3, m2);
+    m3 = SHA256_EXPAND(m1, mc, m4, m3);
+    m4 = SHA256_EXPAND(m2, md, m5, m4);
+    m5 = SHA256_EXPAND(m3, me, m6, m5);
+    m6 = SHA256_EXPAND(m4, mf, m7, m6);
+    m7 = SHA256_EXPAND(m5, m0, m8, m7);
+    m8 = SHA256_EXPAND(m6, m1, m9, m8);
+    m9 = SHA256_EXPAND(m7, m2, ma, m9);
+    ma = SHA256_EXPAND(m8, m3, mb, ma);
+    mb = SHA256_EXPAND(m9, m4, mc, mb);
+    mc = SHA256_EXPAND(ma, m5, md, mc);
+    md = SHA256_EXPAND(mb, m6, me, md);
+    me = SHA256_EXPAND(mc, m7, mf, me);
+    mf = SHA256_EXPAND(md, m8, m0, mf);
 
-    // perform the remaining 48 rounds
-    DEVICE_UNROLL
-    for (int i = 0; i < 16; ++i) {
-        sha256_round(m[i], k[16 + i], a, b, c, d, e, f, g, h);
-    }
-    DEVICE_UNROLL
-    for (int i = 0; i < 16; ++i) {
-        sha256_update_m(m, i);
-        sha256_round(m[i], k[32 + i], a, b, c, d, e, f, g, h);
-    }
-    DEVICE_UNROLL
-    for (int i = 0; i < 16; ++i) {
-        sha256_update_m(m, i);
-        sha256_round(m[i], k[48 + i], a, b, c, d, e, f, g, h);
-    }
+    SHA256_STEP(m0, k[0x10], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m1, k[0x11], h, a, b, c, d, e, f, g);
+    SHA256_STEP(m2, k[0x12], g, h, a, b, c, d, e, f);
+    SHA256_STEP(m3, k[0x13], f, g, h, a, b, c, d, e);
+    SHA256_STEP(m4, k[0x14], e, f, g, h, a, b, c, d);
+    SHA256_STEP(m5, k[0x15], d, e, f, g, h, a, b, c);
+    SHA256_STEP(m6, k[0x16], c, d, e, f, g, h, a, b);
+    SHA256_STEP(m7, k[0x17], b, c, d, e, f, g, h, a);
+    SHA256_STEP(m8, k[0x18], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m9, k[0x19], h, a, b, c, d, e, f, g);
+    SHA256_STEP(ma, k[0x1a], g, h, a, b, c, d, e, f);
+    SHA256_STEP(mb, k[0x1b], f, g, h, a, b, c, d, e);
+    SHA256_STEP(mc, k[0x1c], e, f, g, h, a, b, c, d);
+    SHA256_STEP(md, k[0x1d], d, e, f, g, h, a, b, c);
+    SHA256_STEP(me, k[0x1e], c, d, e, f, g, h, a, b);
+    SHA256_STEP(mf, k[0x1f], b, c, d, e, f, g, h, a);
+
+    m0 = SHA256_EXPAND(me, m9, m1, m0);
+    m1 = SHA256_EXPAND(mf, ma, m2, m1);
+    m2 = SHA256_EXPAND(m0, mb, m3, m2);
+    m3 = SHA256_EXPAND(m1, mc, m4, m3);
+    m4 = SHA256_EXPAND(m2, md, m5, m4);
+    m5 = SHA256_EXPAND(m3, me, m6, m5);
+    m6 = SHA256_EXPAND(m4, mf, m7, m6);
+    m7 = SHA256_EXPAND(m5, m0, m8, m7);
+    m8 = SHA256_EXPAND(m6, m1, m9, m8);
+    m9 = SHA256_EXPAND(m7, m2, ma, m9);
+    ma = SHA256_EXPAND(m8, m3, mb, ma);
+    mb = SHA256_EXPAND(m9, m4, mc, mb);
+    mc = SHA256_EXPAND(ma, m5, md, mc);
+    md = SHA256_EXPAND(mb, m6, me, md);
+    me = SHA256_EXPAND(mc, m7, mf, me);
+    mf = SHA256_EXPAND(md, m8, m0, mf);
+
+    SHA256_STEP(m0, k[0x20], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m1, k[0x21], h, a, b, c, d, e, f, g);
+    SHA256_STEP(m2, k[0x22], g, h, a, b, c, d, e, f);
+    SHA256_STEP(m3, k[0x23], f, g, h, a, b, c, d, e);
+    SHA256_STEP(m4, k[0x24], e, f, g, h, a, b, c, d);
+    SHA256_STEP(m5, k[0x25], d, e, f, g, h, a, b, c);
+    SHA256_STEP(m6, k[0x26], c, d, e, f, g, h, a, b);
+    SHA256_STEP(m7, k[0x27], b, c, d, e, f, g, h, a);
+    SHA256_STEP(m8, k[0x28], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m9, k[0x29], h, a, b, c, d, e, f, g);
+    SHA256_STEP(ma, k[0x2a], g, h, a, b, c, d, e, f);
+    SHA256_STEP(mb, k[0x2b], f, g, h, a, b, c, d, e);
+    SHA256_STEP(mc, k[0x2c], e, f, g, h, a, b, c, d);
+    SHA256_STEP(md, k[0x2d], d, e, f, g, h, a, b, c);
+    SHA256_STEP(me, k[0x2e], c, d, e, f, g, h, a, b);
+    SHA256_STEP(mf, k[0x2f], b, c, d, e, f, g, h, a);
+
+    m0 = SHA256_EXPAND(me, m9, m1, m0);
+    m1 = SHA256_EXPAND(mf, ma, m2, m1);
+    m2 = SHA256_EXPAND(m0, mb, m3, m2);
+    m3 = SHA256_EXPAND(m1, mc, m4, m3);
+    m4 = SHA256_EXPAND(m2, md, m5, m4);
+    m5 = SHA256_EXPAND(m3, me, m6, m5);
+    m6 = SHA256_EXPAND(m4, mf, m7, m6);
+    m7 = SHA256_EXPAND(m5, m0, m8, m7);
+    m8 = SHA256_EXPAND(m6, m1, m9, m8);
+    m9 = SHA256_EXPAND(m7, m2, ma, m9);
+    ma = SHA256_EXPAND(m8, m3, mb, ma);
+    mb = SHA256_EXPAND(m9, m4, mc, mb);
+    mc = SHA256_EXPAND(ma, m5, md, mc);
+    md = SHA256_EXPAND(mb, m6, me, md);
+    me = SHA256_EXPAND(mc, m7, mf, me);
+    mf = SHA256_EXPAND(md, m8, m0, mf);
+
+    SHA256_STEP(m0, k[0x30], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m1, k[0x31], h, a, b, c, d, e, f, g);
+    SHA256_STEP(m2, k[0x32], g, h, a, b, c, d, e, f);
+    SHA256_STEP(m3, k[0x33], f, g, h, a, b, c, d, e);
+    SHA256_STEP(m4, k[0x34], e, f, g, h, a, b, c, d);
+    SHA256_STEP(m5, k[0x35], d, e, f, g, h, a, b, c);
+    SHA256_STEP(m6, k[0x36], c, d, e, f, g, h, a, b);
+    SHA256_STEP(m7, k[0x37], b, c, d, e, f, g, h, a);
+    SHA256_STEP(m8, k[0x38], a, b, c, d, e, f, g, h);
+    SHA256_STEP(m9, k[0x39], h, a, b, c, d, e, f, g);
+    SHA256_STEP(ma, k[0x3a], g, h, a, b, c, d, e, f);
+    SHA256_STEP(mb, k[0x3b], f, g, h, a, b, c, d, e);
+    SHA256_STEP(mc, k[0x3c], e, f, g, h, a, b, c, d);
+    SHA256_STEP(md, k[0x3d], d, e, f, g, h, a, b, c);
+    SHA256_STEP(me, k[0x3e], c, d, e, f, g, h, a, b);
+    SHA256_STEP(mf, k[0x3f], b, c, d, e, f, g, h, a);
 
     hash.arr[0] = aa + a;
     hash.arr[1] = bb + b;
